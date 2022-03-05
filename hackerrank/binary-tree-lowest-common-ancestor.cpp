565843c072b585fa89109c71f69963e58a428970
Siwon  Kim <76824986+Siwon-Kim@users.noreply.github.com>
2022-03-05 13:59:06 -0500

    Create binary-tree-lowest-common-ancestor.cpp

---
+++ hackerrank/binary-tree-lowest-common-ancestor.cpp
@@ -0,0 +1,40 @@
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+*/
+
+Node *lca(Node *root, int v1, int v2) {
+  Node *temp = root;
+  // when v1 or v2 is a root
+  if (root->data == v1 || root->data == v2)
+    return root;
+
+  while (temp != NULL) {
+    // when v1 and v2 are in the opposite sides of the tree
+    if ((v1 > temp->data && v2 < temp->data) ||
+        (v2 > temp->data && v1 < temp->data))
+      return temp;
+    // when v1 and v2 are in the left side
+    if (temp->data > v1 && temp->data > v2) {
+      if (temp->left->data == v1 || temp->left->data == v2) {
+        temp = temp->left;  
+        break;
+      } else {
+        temp = temp->left;
+      }
+    }
+    // when v1 and v2 are in the right side
+    if (temp->data < v1 && temp->data < v2) {
+      if (temp->right->data == v1 || temp->right->data == v2) {
+        temp = temp->right;
+        break;
+      } else {
+        temp = temp->right;
+      }
+    }
+}
+return temp;
+}
