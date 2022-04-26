44f0ff2b91e3628b535092a923f000dcab2e382c
Siwon  Kim <76824986+Siwon-Kim@users.noreply.github.com>
2022-04-26 01:33:43 -0400

    Create 59-subset.cpp

---
+++ inflearn/59-subset.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n, ch[100];
+
+void DFS(int L) {
+  if(L==n+1) {
+    for(int i=1; i<=n; i++) {
+      if(ch[i]==1) cout << i << " ";
+    }
+    cout << "\n";
+  } else {
+    ch[L] = 1;
+    DFS(L+1);
+    ch[L] = 0;
+    DFS(L+1);
+  }
+}
+
+int main() {
+  cin >> n;
+  DFS(1);
+  return 0;
+}
