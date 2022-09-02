0c06129459603135c4a784c1a9300c855896b637
Siwon  Kim <76824986+Siwon-Kim@users.noreply.github.com>
2022-09-01 22:41:43 -0400

    Create 32-selection-sort.cpp

---
+++ inflearn/32-selection-sort.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+  // freopen("input.txt", "rt", stdin);
+  int n;
+  cin >> n;
+  int a[n];
+  for(int i=0; i<n; i++) cin >> a[i];
+  
+  for(int i=0; i<n; i++) {
+    int min = i;
+    for(int j=i+1; j<n; j++) {
+      if(a[min] > a[j]) 
+        min = j;
+    }
+    swap(a[i], a[min]);
+  }
+
+  for(int i=0; i<n; i++) cout << a[i] << " ";
+  
+  return 0;
+}
