e8bbf8427eb9457d658b912d1bdd9c6c68b7e269
Siwon  Kim <76824986+Siwon-Kim@users.noreply.github.com>
2023-01-30 16:35:46 -0500

    Update sorting-3.py

--- this-is-coding-test-python/sorting-3.py
+++ this-is-coding-test-python/sorting-3.py
@@ -1,7 +1,7 @@
 n = int(input())
 array = []
 for _ in range(n):
-  input_data = input().split()
+  input_data = input().split
   array.append((input_data[0], int(input_data[1])))
 
 array = sorted(array, key=lambda student: student[1])
