55954fa2d2827399a3059eda63bce632fcc13e45
Siwon  Kim <76824986+Siwon-Kim@users.noreply.github.com>
2023-01-31 16:44:12 -0500

    Create dp-2.py

---
+++ this-is-coding-test-python/dp-2.py
@@ -0,0 +1,13 @@
+n = int(input())
+m = [0] * 30001
+
+for i in range(2, n + 1):
+  m[i] = m[i - 1] + 1
+  if i % 2 == 0:
+    m[i] = min(m[i], m[i // 2] + 1)
+  if i % 3 == 0:
+    m[i] = min(m[i], m[i // 3] + 1)
+  if i % 5 == 0:
+    m[i] = min(m[i], m[i // 5] + 1)
+
+print(m[n])
