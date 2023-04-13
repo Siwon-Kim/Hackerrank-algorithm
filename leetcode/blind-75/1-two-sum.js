06b2a919533dea1127dd3afa56cb374079321c32
Siwon-Kim <siwonkim1108@gmail.com>
2023-04-13 02:23:01 -0400

    stock

--- leetcode/blind-75/1-two-sum.js
+++ leetcode/blind-75/1-two-sum.js
@@ -22,7 +22,7 @@ var refactoredTwoSum = function(nums, target) {
             return [left, right];
         } else if (right === maxIdx) {
             left++;
-            right = left + 1;
+            right = left + 1; // relocate the right pointer right next to the left pointer
         } else {
             right++;
         }
@@ -34,11 +34,22 @@ var refactoredTwoSum = function(nums, target) {
 var hashMapTwoSum = function(nums, target) {
     const numMap = new Map(); // Hash table to store seen values
     for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (numMap.has(complement)) {
-            return [numMap.get(complement), i];
+        const complement = target - nums[i]; // "complement" refers to the value that needs to be added to the current number in order to obtain the target sum
+        if (numMap.has(complement)) { // find by "key" from table
+            return [numMap.get(complement), i]; // get "value" from table
         }
         numMap.set(nums[i], i);
     }
 };
+
+// in Python
+// def two_sum(nums, target):
+//     num_map = {}  # Dictionary to store seen values
+//     for i in range(len(nums)):
+//         complement = target - nums[i]
+//         if complement in num_map:
+//             return [num_map[complement], i]
+//         num_map[nums[i]] = i
+//     return []  # Return an empty list if target sum is not found
+
 
