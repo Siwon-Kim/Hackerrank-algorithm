7019954eb4638c73f285bbb0b8157c9f8383d5c6
Siwon Kim <siwonkim1108@gmail.com>
2023-07-21 16:12:50 +0900

    Time: 105 ms (34.40%), Space: 49.4 MB (24.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,7 +1,6 @@
 function isAnagram(s: string, t: string): boolean {
     const sortedS = s.split("").sort().join("");
     const sortedT = t.split("").sort().join("");
-    console.log(sortedS, sortedT);
-    if(sortedS === sortedT) return true;
-    else return false;
+    
+    return sortedS === sortedT;
 };
