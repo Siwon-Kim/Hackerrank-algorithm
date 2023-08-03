f6db5f347920d1874a4699e923bc12fe53ed0fd5
Siwon Kim <siwonkim1108@gmail.com>
2023-08-03 22:59:26 +0900

    Delete leetcode directory

--- leetcode/two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        list = []
-        for n in range(0, len(nums)):
-            for m in range(n+1, len(nums)):
-                if((nums[n] + nums[m]) == target):
-                    list.append(n)
-                    list.append(m)
-                    return list
