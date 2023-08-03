c62b9ebf3af9beb39efbdaad06a458a567b897e5
Siwon Kim <siwonkim1108@gmail.com>
2023-08-03 21:49:00 +0900

    Time: 62 ms (91.12%), Space: 43.3 MB (99.18%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    const s1 = s.trim().toLowerCase();
+    const s2 = s1.replace(/[^a-z0-9]/g, "")
+    const len = s2.length;
+    
+    if(len === 0) return true;
+    
+    let [left, right] = [0, len - 1];
+    
+    while(left < right) {
+        if(s2[left] === s2[right]) {
+            left++;
+            right--;
+            continue;
+        }
+        else return false;
+    }
+    
+    return true;
+};
