3d28cd313851b40ecfe352c03f9009ead3c0321c
Siwon-Kim <siwonkim1108@gmail.com>
2023-09-08 16:05:31 -0400

    imp-4-1

--- this-is-coding-test-python/part-2/implementation/imp-4-1.py
+++ this-is-coding-test-python/part-2/implementation/imp-4-1.py
@@ -1,6 +1,19 @@
-# Implementation의 종류
-# 완전탐색 - 모든 경우의 수를 주저 없이 다 계산하는 해결 방법
-# 시뮬레이션 - 문제에서 제시한 알고리즘을 한 단계씩 차례대로 직접 수행
+n = int(input())
+moves = input().split()
 
-# With Python, time lime 1 sec + data 1M, code <= O(NlogN)
+x, y = 1, 1
+dir = ['L', 'R', 'U', 'D']
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
+for move in moves:
+    idx = dir.index(move)
+    tx = x + dx[idx]
+    ty = y + dy[idx]
+
+    if tx < 1 or ty < 1 or tx > n or ty > n:
+        continue
+
+    x, y = tx, ty
+
+print(x, y)
