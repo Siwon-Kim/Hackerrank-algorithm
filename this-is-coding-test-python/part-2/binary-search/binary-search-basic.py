6d547bbd21176651765c00f6395a9df47f69112c
Siwon-Kim <siwonkim1108@gmail.com>
2023-09-11 18:42:54 -0400

    binary search basic

---
+++ this-is-coding-test-python/part-2/binary-search/binary-search-basic.py
@@ -0,0 +1,43 @@
+# Sequetial Search: O(N)
+# Binary Search: O(logN)
+
+# Binary Search
+# Data should be sorted
+# Set start, end, middle points
+# Keep comparing the target and the middle value
+
+# recursion
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) / 2
+
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return binary_search(array, target, mid + 1, end)
+    
+n, target = list(map(int, input().split()))
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n - 1)
+if result == None:
+    print("No target exists")
+else:
+    print("At index ", result + 1)
+
+
+# for loop
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) / 2
+
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
