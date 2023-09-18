0e5d8217d6e550f6c1fb0df4f8b54215b0076255
Siwon-Kim <siwonkim1108@gmail.com>
2023-09-18 18:54:31 -0400

    bs-7-3

--- this-is-coding-test-python/part-2/binary-search/bs-7-3.py
+++ this-is-coding-test-python/part-2/binary-search/bs-7-3.py
@@ -5,3 +5,20 @@
 N, M = map(int, input().split())
 height = list(map(int, input().split()))
 
+result = 0
+start = 0
+end = max(height)
+
+while start <= end:
+    mid = (start + end) // 2
+    sum = 0
+    for i in height:
+        if i - mid > 0:
+            sum += i - mid
+    if M > sum:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
