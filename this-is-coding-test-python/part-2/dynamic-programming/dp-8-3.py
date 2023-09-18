12e9a8101b166f74d878c3582c5550dab4f02795
Siwon-Kim <siwonkim1108@gmail.com>
2023-09-18 19:40:36 -0400

    dp-8-3

---
+++ this-is-coding-test-python/part-2/dynamic-programming/dp-8-3.py
@@ -0,0 +1,12 @@
+N = int(input())
+arr = list(map(int, input().split()))
+
+d = [0] * 101
+
+d[0] = arr[0]
+d[1] = max(arr[0], arr[1])
+
+for i in range(2, N):
+    d[i] = max(d[i - 1], d[i - 2] + arr[i])
+
+print(d[N - 1])
