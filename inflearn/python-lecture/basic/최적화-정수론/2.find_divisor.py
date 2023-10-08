6744cdc954e09d70a71257d238f7244465e10e39
Siwon-Kim <siwonkim1108@gmail.com>
2023-10-08 15:00:00 -0400

    finding divisor only until the roof of n

--- inflearn/python-lecture/basic/최적화-정수론/2.find_divisor.py
+++ inflearn/python-lecture/basic/최적화-정수론/2.find_divisor.py
@@ -1,9 +1,11 @@
 n = int(input())
 
 answer = []
-for i in range(2, n // 2 + 1):
+for i in range(2, int(n ** 0.5) + 1):
     if n % i == 0:
         answer.append(i)
+        if n // i != i:
+            answer.append(n // i)
 
 print(len(answer))
 print(*answer)
