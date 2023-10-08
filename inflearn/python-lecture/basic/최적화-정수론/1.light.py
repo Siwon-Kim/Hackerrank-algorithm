af287f3e38393a7af74f002dc48c28f871a1448c
Siwon-Kim <siwonkim1108@gmail.com>
2023-10-08 14:46:07 -0400

    1.light - no optimization

---
+++ inflearn/python-lecture/basic/최적화-정수론/1.light.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+lights = [0] * (n + 1) # 0 - green, 1 - red
+
+# green -> red -> green 
+# if n == 24
+# 1 -> press 24
+# 2 -> press 12
+# 3 -> press 8
+# 4 -> press 6
+# 5 -> press 4
+# 6 -> press 4
+# 7 -> press 3
+# ...
+# 24 -> press 1
+
+for i in range(1, n + 1): # person
+    for j in range(i, n + 1): # light number
+        if j % i == 0:
+            lights[j] = 1 if lights[j] == 0 else 0
+
+print(lights)
+answer = 0
+for i in range(1, n + 1):
+    if lights[i] == 1:
+        answer += 1
+
+print(answer)
