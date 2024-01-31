31d4089e68c67174b73622b5d9a1544a40e97efa
Siwon-Kim <siwonkim1108@gmail.com>
2024-01-31 13:47:42 +0900

    2304 텐트 세우기

--- inflearn/python-lecture/basic/기억-누적합/2304-텐트세우기.py
+++ inflearn/python-lecture/basic/기억-누적합/2304-텐트세우기.py
@@ -1,11 +1,32 @@
-n = int(input())
-array = []
+import sys
+input = sys.stdin.readline
 
-for _ in range(n):
-    val = list(map(int, input().split()))
-    array.append(val)
+max_height = 0
+max_idx = 0
+col = [0 for _ in range(1001)] # 기둥
 
-# 내부 리스트의 첫 번째 요소를 기준으로 정렬
-array.sort(key=lambda x: x[0])
+# 기둥 정보 저장하기
+for _ in range(int(input())):
+    l, h = list(map(int, input().split()))
+    col[l] = h
+    if max_height < h:
+        max_idx = l
+        max_height = h
 
-print(array)
+curr = 0
+answer = 0
+
+# 왼쪽 기둥 그룹
+for i in range(max_idx+1):
+    curr = max(curr, col[i])
+    answer += curr
+
+curr = 0
+
+# 오른쪽 기둥 그룹
+for i in range(1000, max_idx, -1):
+    curr = max(curr, col[i])
+    answer += curr
+
+print(answer)
+
