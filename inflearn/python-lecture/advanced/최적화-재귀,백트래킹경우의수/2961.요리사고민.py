10a045571dd273d05f9302f3fb291e5e200433da
Siwon-Kim <siwonkim1108@gmail.com>
2024-02-01 12:58:23 +0900

    recursion with cases

---
+++ inflearn/python-lecture/advanced/최적화-재귀,백트래킹경우의수/2961.요리사고민.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+sys.setrecursionlimit(10 * 6)
+
+def recur(idx, sour, bitter, used):
+    global answer
+
+    if idx == n:
+        if used > 0: # 아무 재료도 사용하지 않은 경우
+            answer = min(abs(bitter - sour), answer)
+        return
+    
+    recur(idx+1, sour*ingre[idx][0], bitter+ingre[idx][1], used+1) # 재료를 선택할 경우
+    recur(idx+1, sour, bitter, used) # 재료를 선택하지 않은 경우
+
+
+n = int(input())
+ingre = [list(map(int, input().split())) for _ in range(n)]
+
+answer = int(1e9)
+
+recur(0, 1, 0, 0)
+
+print(answer)
