262757877d133633fe3dd1623756c7952d8f9b47
Siwon-Kim <siwonkim1108@gmail.com>
2024-02-01 09:49:20 +0900

    permutation recursion

--- inflearn/python-lecture/advanced/완전탐색-재귀,백트래킹/수열만들기.py
+++ inflearn/python-lecture/advanced/완전탐색-재귀,백트래킹/수열만들기.py
@@ -5,5 +5,20 @@ n, m = map(int, input().split())
 
 # 반복문
 for i in range(1, n+1):
-    for j in range(i+1, n+1):
+    for j in range(i, n+1):
         print(i, j)
+
+print("\n\n")
+
+# 재귀함수
+def recur(i, n):
+    if i > n:
+        return
+    
+    for j in range(i, n+1):
+        print(i, j)
+
+    recur(i+1, n)
+
+arr = []
+recur(1, n)
